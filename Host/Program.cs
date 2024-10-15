666ae00339c437e5f2f377df6a5291db8007d4af
AxelVC22 <axlvaldez74@gmail.com>
2024-10-14 22:36:56 -0600

    Commit inicial

---
+++ Host/Program.cs
@@ -0,0 +1,20 @@
+﻿using ChatService;
+using System;
+using System.ServiceModel;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ChatService.ChatServiceImplementation)))
+            {
+                host.Open();
+                Console.WriteLine("Server is running");
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
